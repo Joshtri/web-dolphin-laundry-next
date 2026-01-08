9d89c81aa01bdbdf39c59ba2904c7656ce2079ea
Joshtri <stuffofyos1516@gmail.com>
2026-01-08 22:38:57 +0700

    fix changed used form env

--- next.config.ts
+++ next.config.ts
@@ -13,7 +13,7 @@ const nextConfig: NextConfig = {
     return [
       {
         source: "/api/:path*",
-        destination: "https://landing-page-dolphin-api.vercel.app/api/:path*",
+        destination: `${process.env.NEXT_PUBLIC_API_URL}/:path*`,
       },
     ];
   },
